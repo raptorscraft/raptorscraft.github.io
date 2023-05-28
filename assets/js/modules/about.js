d75589d93849116dbd42696bd8474a4b9e8a0201
raptorscraft <raptorscraft@mail.ru>
2023-05-28 17:24:51 +0300

    add main, rules and about pages

--- assets/js/modules/about.js
+++ assets/js/modules/about.js
@@ -1,5 +1,18 @@
 export function toAbout(forLogo, forInfo) {
     forLogo.innerHTML = "About";
 
-    forInfo.innerHTML = `About`;
+    forInfo.innerHTML = `
+    Cвязь с администрацией:<br>
+    <i class="fa-brands fa-vk"></i> <a class = "social_link" href = "https://vk.com/aovent" target = "_blank">VK</a> / <a class = "social_link" href = "https://vk.com/ya_gbc" target = "_blank">VK</a>
+    <br>
+    <i class="fa-brands fa-telegram"></i> <a href = "https://t.me/greydatlenght" class = "social_link" target = "_blank">Telegram</a> / <a href = "https://t.me/FanTheG" class = "social_link" target = "_blank">Telegram</a>
+    <br><br>
+    Остальные места, связанные с нашим проектом:<br>
+    <i class="fa-brands fa-discord"></i> <a href = "https://discord.gg/j7Fb3mRTMN" target = "_blank" class = "social_link">Discord</a>
+    <br><br>
+    Специальные источники:<br>
+    <i class="fa-solid fa-robot"></i> Генерация текстов, поиск материалов и помощь в разработке - <a href = "https://chat.openai.com/" target = "_blank" class = "social_link">ChatGPT</a>
+    <br><br>
+    <span style = "font-size: 0.7vh; color: yellow;">Информация будет пополняться</span>
+    `;
 }
--- assets/js/modules/raptorscraft.js
+++ assets/js/modules/raptorscraft.js
@@ -1,7 +1,11 @@
 export function toRaptorscraft(forLogo, forInfo) {
     forLogo.innerHTML = "WELCOME TO THE SERVER!";
 
-    forInfo.innerHTML = `<span class = 'RaptorsCraft'>RaportsCraft</span> - Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sit eaque iure alias odit enim possimus pariatur ullam inventore, nihil molestias. In molestias deleniti sequi sapiente mollitia iusto. Cupiditate, corrupti!
-    Veritatis similique impedit molestias rerum earum natus harum fuga, eveniet porro officiis commodi tempora quam, obcaecati asperiores animi iste? Pariatur officia laudantium quaerat obcaecati voluptates eius consequatur veniam totam ullam.
-    Possimus deserunt, tempore facere cupiditate sunt enim itaque est eveniet labore voluptatum neque laudantium nobis quis aliquid amet accusantium architecto. Illum quis tenetur voluptatem corrupti provident molestias dolore dignissimos tempora.`;
+    forInfo.innerHTML = `
+    <span class = "RaptorsCraft"><a class = "RaptorsCraft" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">RaportsCraft</a></span> - сообщество, где социальное взаимодействие является основной ценностью. У нас ты найдешь уникальный игровой опыт без доната и лишних ограничений.
+    <br><br>
+    На нашей веб-странице ты найдешь все необходимые сведения о RaportsCraft. В разделе "RaportsCraft" ты сейчас находишься и можешь прочитать общую информацию о нашем сервере. В разделе "About" мы предоставляем ссылки, контактную информацию и другие полезные ресурсы для тебя. Раздел "Rules" содержит правила поведения на сервере. А раздел "How To Play" поможет тебе попасть на наш сервер и начать играть.
+    <br><br>
+    Наш сервер открывается 1 июля 2023 года и будет доступен до 31 августа 2023 года.
+    `
 }
