f2746145145ff07a655193a3ef55b55af6651b07
raptorscraft <134744893+raptorscraft@users.noreply.github.com>
2023-06-02 15:41:43 +0300

    Update howtoplay.js

--- assets/js/modules/howtoplay.js
+++ assets/js/modules/howtoplay.js
@@ -45,6 +45,7 @@ export function toHowtoplay(forLogo, forInfo) {
                 <br>
                 <span class = "command">/delete_clan [ID]</span> - позволяет пользователю удалить свой клан по его айди.
                 <br><br>
+                <span class = "command">;transfer-clan-ownership [ID / mention]</span> - передать пользователю права владельца клана.
                 У каждого клана есть своя отдельная роль, голосовой и текстовый каналы. Они видны и могут использоваться только участниками клана.
                 Участних может находиться только в одном клане.
             </div>
